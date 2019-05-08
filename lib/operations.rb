804f40d3b3c9186ab2380a0d097936c3333bbacb
Kurtis Leach <danceflurry4366@gmail.com>
2019-05-08 23:34:54 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,14 @@
 def unsafe?(speed)
-	if speed < 40
-	true
-	elseif speed > 60
-	  true
-	else
-	  false
+	if speed > 60
+			return true
+		elsif speed < 40
+			return true
+		else return false
 	end
 
 
 def not_safe?(speed)
-	speed < 40 ? || speed > 60 true : false
+	speed > 60 ? true : false
+speed < 40 ? true : false
+speed < 60 && speed > 40 ? false : true
 end
